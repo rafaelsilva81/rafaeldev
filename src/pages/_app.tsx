7b313bbe7a539e64f2e6ed30edcc07d42fe867ab
Rafael Galdino da Silva <rafaelgaldinosilva81@gmail.com>
2022-11-09 17:49:40 -0300

    style: adding favicon and og:image meta tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -24,6 +24,58 @@ export default function App({ Component, pageProps }: AppProps) {
           name='keywords'
           content='portoflio, webdev, web, react, next, developer, programmer'
         />
+        <meta
+          property='og:site_name'
+          content='Rafael Galdino da Silva - Portfolio'
+        />
+        <meta
+          property='og:title'
+          content='Rafael Galdino da Silva - Portfolio'
+        />
+        <meta
+          property='og:description'
+          content='Rafael Galdino da Silva, Fullstack Developer, NodeJS, ReactJS, NextJS, Typescript and more'
+        />
+        <meta
+          property='og:image'
+          itemProp='image'
+          content='https://i.imgur.com/iqA94gK.png'
+        />
+
+        <link
+          rel='apple-touch-icon'
+          sizes='180x180'
+          href='/apple-touch-icon.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href='/favicon-32x32.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+          href='/favicon-16x16.png'
+        />
+        <link
+          rel='manifest'
+          href='/site.webmanifest'
+        />
+        <link
+          rel='mask-icon'
+          href='/safari-pinned-tab.svg'
+          color='#f8f8f8'
+        />
+        <meta
+          name='msapplication-TileColor'
+          content='#121212'
+        />
+        <meta
+          name='theme-color'
+          content='#ffffff'
+        />
       </Head>
       <Script
         async
