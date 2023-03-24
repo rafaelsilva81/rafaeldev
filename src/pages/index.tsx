9d35a7b80999d2cfa8cb50bc44703e92d356e24b
Rafael Galdino da Silva <rafaelgaldinosilva81@gmail.com>
2023-03-24 17:16:05 -0300

    feat: add new resume, improving the layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,27 +13,25 @@ export default function Index() {
 
   return (
     <ClientOnly className={prefersDark ? "dark" : "light"}>
-      <div className="p-5 bg-light dark:bg-dark">
-        <div className="grid min-h-screen gap-4 mx-auto max-w-7xl md:gap-6">
-          {/* Top Bar section */}
-          <div className="flex flex-col items-start justify-between gap-4 md:flex-row md:items-center">
-            <HomeHeader />
-          </div>
+      <div className="grid w-full min-h-screen gap-4 px-24 py-4 md:gap-6 bg-light dark:bg-dark">
+        {/* Top Bar section */}
+        <div className="flex flex-col items-start justify-between gap-4 md:flex-row md:items-center">
+          <HomeHeader />
+        </div>
 
-          {/* Main content section */}
-          <main className="flex flex-1">
-            <PortfolioContent />
-          </main>
+        {/* Main content section */}
+        <main className="flex flex-1">
+          <PortfolioContent />
+        </main>
 
-          {/* Bottom Bar section PC */}
-          <div className="items-center justify-between hidden md:flex">
-            <HomeFooter />
-          </div>
+        {/* Bottom Bar section PC */}
+        <div className="items-center justify-between hidden md:flex">
+          <HomeFooter />
+        </div>
 
-          {/* Bottom Bar section Mobile */}
-          <div className="flex flex-col gap-2 md:hidden">
-            <HomeFooter />
-          </div>
+        {/* Bottom Bar section Mobile */}
+        <div className="flex flex-col gap-2 md:hidden">
+          <HomeFooter />
         </div>
       </div>
     </ClientOnly>
